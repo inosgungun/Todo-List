12920ed0c66216cc691ed0bbdc6cde052f5b97e9
Gungun Soni <gungunbps2018@gmail.com>
2025-06-03 00:16:03 +0530

    Managed session storage

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN" || (e.target.tagName === "IMG" && e.target.parentElement.tagName === "SPAN")) {
+        e.target.closest("li").remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
